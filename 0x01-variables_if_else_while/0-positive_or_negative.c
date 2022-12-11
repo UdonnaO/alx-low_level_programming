7dbe494b8ccb5b344acfcf55b9c9ccf683293fb2
Udonna <udonnaokebata@gmail.com>
2022-12-11 09:57:52 +0000

    positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /**
  * main - Entry point
- * Description - Prints if random generated number is positive, zero or negative
+ * Description - Prints if random generated number is +ve, zero or -ve
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -21,5 +21,4 @@ int main(void)
 	else
 		printf("%d is positive\n", n);
 	return (0);
-
 }
