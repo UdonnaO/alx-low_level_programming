b3e0bc1c44922f2c07ddf933ee1c934620b49975
Udonna <udonnaokebata@gmail.com>
2022-12-23 05:01:28 +0000

    6 cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,9 +2,9 @@
 
 /**
  *  * cap_string - Capitalizes all words of a string.
- *   * @str: The string to be capitalized.
- *    *
- *     * Return: A pointer to the changed string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
 {
